b4582689f687ce31958c8c02cb744c1aef38f39f
Daniel Bezerra <dbezerra@desenbahia.ba.gov.br>
2021-11-19 14:29:33 -0300

    Ajustes para passar o Analyze

--- lib/commonComponents/custom_card.dart
+++ lib/commonComponents/custom_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  CustomCard({@required this.index});
+  // ignore: use_key_in_widget_constructors
+  const CustomCard({@required this.index});
+  // ignore: prefer_typing_uninitialized_variables
   final index;
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'commonComponents/custom_card.dart';
+// import 'commonComponents/custom_card.dart';
 
 void main() {
   runApp(const MyApp());
